a2cd2d1c7f423b6672a48a854fab95969e74da80
do <thanhdo53th@gmail.com>
2018-09-30 11:32:36 +0700

    php

---
+++ create_token.php
@@ -0,0 +1,29 @@
+<?php
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"http://52.243.57.222/api/v1/wallet/transaction/create_token/");
+//curl_setopt($ch, CURLOPT_URL,"https://demo1-api.infd.jp/api/v1/wallet/transaction/create_token/");
+curl_setopt($ch, CURLOPT_POST, 1);
+$payment_method_id = isset($_POST["payment_method_id"]) ? $_POST["payment_method_id"] : "";
+$vars = [
+    "payment_method_id" => $payment_method_id
+];
+//curl_setopt($ch, CURLOPT_POSTFIELDS, $vars);  //Post Fields
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($vars));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$headers = [
+    'Cache-Control: no-cache',
+    'Content-Type: application/json',
+    'X-Wallet-OS: 5.1',
+    'OS: Android',
+    'X-Wallet-OS-Level: 23',
+    'X-Wallet-App-Ver: 1.00.00',
+    'Authorization: Bearer 2c0acf13e1b457863f65a74bde0de50a3cff583365b969c4b39a72bcb1d87f08',
+    'Accept: application/json',
+    'User-Agent: resona-wallet-app'
+];
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$server_output = curl_exec ($ch);
+curl_close ($ch);
+
+echo  $server_output ;
